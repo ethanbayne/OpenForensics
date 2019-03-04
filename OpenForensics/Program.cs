6d06b4d09457dea0d1fb79546447e0b4bb134d93
Ethan Bayne <ethanbayne@live.co.uk>
2019-03-04 14:11:23 +0000

    Enable admin check (disabled for debug)

--- OpenForensics/Program.cs
+++ OpenForensics/Program.cs
@@ -15,35 +15,35 @@ namespace OpenForensics
             * The user is the administrator of time, direct start application
             * If not the administrator, use the startup object start program, using run as administrator to ensure
             */
-            ////Get the current logged on user labeled Windows
-            //System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
-            //System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
-            ////Judge whether the currently logged in user.
-            //if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
-            //{
-            //    //If the administrator, is directly run
+            //Get the current logged on user labeled Windows
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
+            //Judge whether the currently logged in user.
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                //If the administrator, is directly run
                 Application.Run(new OpenForensics());
-            //}
-            //else
-            //{
-            //    //Create a startup object
-            //    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            //    startInfo.UseShellExecute = true;
-            //    startInfo.WorkingDirectory = Environment.CurrentDirectory;
-            //    startInfo.FileName = Application.ExecutablePath;
-            //    //Set the start action, make sure to run as Administrator
-            //    startInfo.Verb = "runas";
-            //    try
-            //    {
-            //        System.Diagnostics.Process.Start(startInfo);
-            //    }
-            //    catch
-            //    {
-            //        return;
-            //    }
-            //    //Sign out
-            //    Application.Exit();
-            //}
+            }
+            else
+            {
+                //Create a startup object
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.UseShellExecute = true;
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = Application.ExecutablePath;
+                //Set the start action, make sure to run as Administrator
+                startInfo.Verb = "runas";
+                try
+                {
+                    System.Diagnostics.Process.Start(startInfo);
+                }
+                catch
+                {
+                    return;
+                }
+                //Sign out
+                Application.Exit();
+            }
         }
     }
 }
